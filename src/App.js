13aff16f06203042c33371ff27049e5afed0e165
unknown <justiiturraspe@gmail.com>
2022-07-15 03:43:32 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="App">
+      < NavBar />
       <header className="App-header">
         <h1>Hola mundo</h1>
       </header>
