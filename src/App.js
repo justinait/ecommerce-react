825c68e420af21b7c605ab0733f12e4528e560b4
unknown <justiiturraspe@gmail.com>
2022-07-20 03:41:39 -0300

    itemListContainer set with props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar'
+import ItemListContainer from './components/ItemListContainer'
 
 function App() {
   return (
     <div className="App">
       < NavBar />
+      <ItemListContainer greeting="Más vendidos"/>
+      <ItemListContainer greeting="Más recientes"/>
+      <ItemListContainer greeting="Premiados"/>
     </div>
   );
 }
