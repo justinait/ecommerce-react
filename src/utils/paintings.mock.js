153f296d3cc919293082bb8c63d8bdae11a0fa0a
unknown <justiiturraspe@gmail.com>
2022-07-27 01:09:06 -0300

    reset

---
+++ src/utils/paintings.mock.js
@@ -0,0 +1,41 @@
+const paintings = [
+  {
+    "id": 1,
+    "title": "Puente colgante",
+    "high": "60",
+    "width": "90",
+    "category": "landscape",
+    "mainImage": "/images/paintings/landscape/puenteColgante.jpg"
+  },
+  {
+    "id": 2,
+    "title": "Tilcara",
+    "high": "60",
+    "width": "90",
+    "mainImage": "/images/paintings/landscape/tilcara.jpg"
+  },
+  {
+    "id": "3",
+    "title": "y si me dejas un lugarcito",
+    "high": "70",
+    "width": "90",
+    "serieName": "",
+    "mainImage": "/images/paintings/landscape/1624521295580.jpg"
+  },
+  {
+    "title": "violinista",
+    "high": "50",
+    "width": "30",
+    "mainImage": "/images/paintings/humanFigure/1624401158097.jpg",
+    "id": 4
+  },
+  {
+    "title": "espejo",
+    "high": "60",
+    "width": "100",
+    "mainImage": "/images/paintings/stillLife/1624603001642.jpg",
+    "id": 5
+  }
+]
+
+export default paintings;
