9108f589dde66870d1c11cdc7a7d1d914cb7101b
unknown <justiiturraspe@gmail.com>
2022-08-04 23:40:08 -0300

    navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavDropdown } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -12,9 +13,13 @@ function NavBar() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/products">Productos</Nav.Link>
+              <Nav.Link href="/category">Productos</Nav.Link>
               <Nav.Link href="#about">Sobre nosotros</Nav.Link>
               <Nav.Link href="#contact">Contacto</Nav.Link>
+              <NavDropdown title="Categoria" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/category/pet">Plastico</NavDropdown.Item>
+                <NavDropdown.Item href="/category/vidrio">Vidrio</NavDropdown.Item>
+              </NavDropdown>
             </Nav>
           </Navbar.Collapse>
         </Container>
