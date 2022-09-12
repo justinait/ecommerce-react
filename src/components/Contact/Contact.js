2e4074ec71d4363c230717042536a559561113f7
unknown <justiiturraspe@gmail.com>
2022-09-12 19:06:04 -0300

    adding css about y contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import '../About/About.css'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className='container'>
+      <img src="https://www.ecured.cu/images/thumb/3/38/Aceituna_1.jpg/260px-Aceituna_1.jpg" alt="" />
+      <h1>Contactanos!</h1>
+      <h3>Whatsapp: 12345678</h3>
+      <h3>Email: aceitedeoliva@gmail.com</h3>
+      <h3>Dirección: Calle Aceite 123</h3>
+    </div>
   )
 }
 
