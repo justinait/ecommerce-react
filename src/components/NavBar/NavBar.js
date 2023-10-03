bf756954e88851ad0d8456e0e66b00212dd7728c
justinait <justiiturraspe@gmail.com>
2023-10-03 15:42:43 -0300

    navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,32 +1,46 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { CartContext } from '../../context/CartProvider';
 import CartWidget from './CartWidget';
-import './NavBar.css'
+import './NavBar.css';
 
 function NavBar() {
-
   const { totalProducts } = useContext(CartContext);
+  const [showSubMenu, setShowSubMenu] = useState(false);
+
+  const toggleSubMenu = () => {
+    setShowSubMenu(!showSubMenu);
+  };
 
   return (
     <div className="navbar">
-      <NavLink style = {{textDecoration: "none", color: 'black'}} to='/'><h1>oliva litoral</h1></NavLink>
+      <NavLink style={{ textDecoration: 'none', color: 'black' }} to="/">
+        <h1>oliva litoral</h1>
+      </NavLink>
 
-      <div className='rightNavbar'>
+      <div className="rightNavbar">
+        <div
+          className={`navbarOptions ${showSubMenu ? 'active' : ''}`}
+          onClick={toggleSubMenu}
+        >
+          Productos
+          {showSubMenu && (
+            <div className="submenu">
+              <NavLink to="/category/aceitunas">Aceitunas</NavLink>
+              <NavLink to="/category/aceite">Aceite de Oliva</NavLink>
+            </div>
+          )}
+        </div>
+        <NavLink className="navbarOptions" to="/about">
+          Nosotros
+        </NavLink>
+        <NavLink className="navbarOptions" to="/contact">
+          Contacto
+        </NavLink>
 
-        <NavLink className='navbarOptions' to='/category'>Productos</NavLink>
-        <NavLink className='navbarOptions' to='/category/aceitunas'>Aceitunas</NavLink>
-        <NavLink className='navbarOptions' to='/category/aceite'>Aceite de Oliva</NavLink>  
-        <NavLink className='navbarOptions' to='/about'>Nosotros</NavLink>
-        <NavLink className='navbarOptions' to='/contact'>Contacto</NavLink>
-
-        {          
-          totalProducts ? <CartWidget/> : ''
-        }
-
+        {totalProducts ? <CartWidget /> : ''}
       </div>
     </div>
-    
   );
 }
 
